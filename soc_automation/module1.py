af7ec116f1bb9107940fbd3561c16f4647b6ed9f
Deepak <102180983+karkideepak@users.noreply.github.com>
2026-03-24 15:49:39 -0500

    Update module1.py

--- soc_automation/module1.py
+++ soc_automation/module1.py
@@ -5,5 +5,16 @@ THRESHOLD = 30
 
 ip_counter = Counter()
 
-with open("traffic.csv", newline-"") as csvfile;
-  reader = src_ip
+with open("traffic.csv", newline-"") as csvfile
+  reader = csv.DictReader(csvfile)
+
+  for row in reader:
+    src_ip = row.get("ip.src")
+    
+    if src_ip:
+      ip_counter[sc_ip] +=1
+      
+print ("traffic voolume per source:\n")
+
+for ip, count in ip_counter.items():
+  print(f"{ip}: {count} packets)
